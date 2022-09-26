64348b77625b8f7ea34308447f469489a60c0b7c
Maksim Samuilionak <maximwow70@gmail.com>
2022-09-26 22:10:20 +0400

    js lesson 3

--- index.js
+++ index.js
@@ -1,74 +1,45 @@
-// let currentUserName = "Maksim";
-// console.log(currentUserName);
-
-// const otherCurrentUserName = "Aleh";
-// console.log(otherCurrentUserName);
-
-// // asd asdakl lzclkzklx cklmzxlkl cmzx otherCurrentUserName = "2";
-// currentUserName = "Maksim 2";
-
-// console.log(currentUserName);
-// console.log(otherCurrentUserName);
-
-// console.log(userName);
-// var userName = "12312312";
-
-// currentUserName = 12;
-// console.log(currentUserName);
-
-// currentUserName = null;
-// console.log(currentUserName);
-
-// const user = {
-//   id: 1,
-//   name: otherCurrentUserName,
-// };
-// console.log(user);
-
-// const isUserLogged = false;
-// if (!isUserLogged && true) {
-//   //
-//   //
-//   //
-//   console.log("User logged!");
-// } else if (true) {
-//   console.log("User should be logged!");
-// } else {
-//   //
-// }
-
-// console.error(
-//   !isUserLogged && true ? "User logged!" : "User should be logged!"
-// );
-
-// let a = 0;
-// console.log(a++); // выводит 0 все верно.
-// // a = 1
-// console.log(++a); // выводит 2 вместо 1.
-
-// const users = [
-//   user,
-//   {
-//     id: 2,
-//     name: "custom user",
-//   },
-// ];
-
-// // for (let i = 0; i < users.length; i++) {
-// //   console.log(users[i].name);
-// // }
-// const filteredUser = users
-//   .filter((user) => user.name === "Aleh")
-//   .forEach((user) => console.log(user));
-
-if (1 || (0 && 5) || 0) {
+function Hotel(id, name, type, caption) {
+  this.id = id;
+  this.name = name;
+  this.type = type;
+  this.caption = caption;
 }
 
-function concatString(str, otherStr) {
-  return `${str} plus ${otherStr}`;
+const hotel1 = new Hotel(0, "Hotel 3", "city", "<div>hello</div>");
+const hotel2 = new Hotel(1, "Hotel 42", "other city", "<div>hello</div>");
+const hotel3 = new Hotel(2, "hotel 22", "another city", "<div>hello</div>");
+
+const hotelList = [hotel1, hotel3, new Hotel(3, undefined, "city", "")];
+hotelList.push(hotel2);
+console.log(hotelList);
+
+const cityHotelNames = hotelList
+  .filter((hotel) => hotel.type === "city")
+  .map(function (hotel) {
+    return hotel.name;
+  });
+console.log(cityHotelNames);
+
+const hotel3Find = hotelList.find((hotel) => hotel.name === "Hotel 3123");
+if (hotel3Find) {
+  console.log(hotel3Find?.name);
 }
 
-const concatedStrings = concatString("q", "2");
-console.log(concatedStrings);
+hotelList
+  .map((h) => h.name)
+  .forEach((name) => {
+    console.log(name);
+  });
 
+const condition = hotelList.some((hotel) => hotel.id === -2);
+console.log(condition);
 
+const newList = hotelList
+  .filter((h) => Boolean(h.name))
+  .sort((prevHotel, nextHotel) => {
+    const prevName = parseInt(prevHotel.name.split(" ")[1]);
+    const nextName = parseInt(nextHotel.name.split(" ")[1]);
+    return nextName - prevName;
+  });
+
+console.log(newList);
